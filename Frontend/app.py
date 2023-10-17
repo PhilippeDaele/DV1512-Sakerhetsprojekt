09fa9536bd5aaf32093398479a9b589cff2bc6bb
sanqian <2531607953@qq.com>
2023-10-17 10:52:37 +0200

    Login with sqlite

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, url_for, request 
+import sqlite3 
 
 app = Flask(__name__)
 
@@ -7,9 +7,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+connect = sqlite3.connect('database.db') 
+connect.execute('CREATE TABLE IF NOT EXISTS t_users (uname TEXT, password TEXT)') 
+
+@app.route('/login',methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST': 
+        uname = request.form['uname'] 
+        password = request.form['password'] 
+
+        connect = sqlite3.connect('database.db') 
+        cursor = connect.cursor() 
+        cursor.execute('SELECT * FROM t_users WHERE `uname` = ? AND `password` = ?',(uname,password)) 
+        data = cursor.fetchall()
+        if len(data) != 0:
+            return render_template('login.html',success = True)
+        else:
+            return render_template('login.html') 
+    else: 
+        return render_template('login.html') 
 
 @app.route('/logger')
 def logger():
